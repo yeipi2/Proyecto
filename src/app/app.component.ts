e2cf917d5705ce79393ca9b53cec38535f2de666
yeipi2 <elyeipi23@gmail.com>
2024-02-27 17:42:44 -0700

    se agrega el proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  
+  title = 'Pagina Principal';
+card ={
+  title : 'Te Ganste Un PS5',
+  descripcion : 'Me llena de alegría y emoción por la obtención de una PlayStation 5. ',
+
+}
+
+vent ={
+ title : 'JAJAJAJAJ',
+ boton1 : 'Virus',
+ boton2 : 'Cancelar(virus)',
+}
+
+
+}
