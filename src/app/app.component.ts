52dfa958fc6256b8061dcf32944158d2ac9743a9
yeipi2 <elyeipi23@gmail.com>
2024-02-29 10:37:38 -0700

    se creo varios componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { PrimerDiaComponent } from './primer-dia/primer-dia.component';
 import { SegundoDiaComponent } from './segundo-dia/segundo-dia.component';
 import { TercerDiaComponentComponent } from './tercer-dia-component/tercer-dia-component.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,PrimerDiaComponent,SegundoDiaComponent,TercerDiaComponentComponent],
+  imports: [RouterOutlet,PrimerDiaComponent,SegundoDiaComponent,TercerDiaComponentComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { PrimerDiaComponent } from './primer-dia/primer-dia.component';
+import { SegundoDiaComponent } from './segundo-dia/segundo-dia.component';
+import { TercerDiaComponentComponent } from './tercer-dia-component/tercer-dia-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+ path: '',
+ component:PrimerDiaComponent
+},
+{
+path:'primer-dia',
+component:PrimerDiaComponent
+},
+{
+ path:'segundo-dia',
+ component:SegundoDiaComponent
+},
+{
+    path:'tercer-dia',
+    component:TercerDiaComponentComponent
+},
+{
+    path:'**',
+    component:PrimerDiaComponent
+}
+   
+   
+
+
+];
